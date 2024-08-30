ec2d4008c1701388e01f6c55b5aaf972193969cf
Kevin McConnell <kevin@37signals.com>
2024-08-30 09:26:28 +0100

    Test the body content of error page responses

--- internal/server/error_page_middleware_test.go
+++ internal/server/error_page_middleware_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestErrorPageMiddleware(t *testing.T) {
-	check := func(handler http.HandlerFunc) (int, string) {
+	check := func(handler http.HandlerFunc) (int, string, string) {
 		middleware := WithErrorPageMiddleware(handler)
 
 		req := httptest.NewRequest("GET", "http://example.com", nil)
@@ -17,29 +17,42 @@ func TestErrorPageMiddleware(t *testing.T) {
 
 		middleware.ServeHTTP(resp, req)
 
-		return resp.Result().StatusCode, resp.Header().Get("Content-Type")
+		return resp.Result().StatusCode, resp.Header().Get("Content-Type"), resp.Body.String()
 	}
 
 	t.Run("When setting a custom error response", func(t *testing.T) {
-		status, contentType := check(func(w http.ResponseWriter, r *http.Request) {
+		status, contentType, body := check(func(w http.ResponseWriter, r *http.Request) {
 			SetErrorResponse(w, r, http.StatusNotFound, nil)
 		})
 
 		assert.Equal(t, http.StatusNotFound, status)
 		assert.Equal(t, "text/html; charset=utf-8", contentType)
+		assert.Regexp(t, "Not Found", body)
+	})
+
+	t.Run("When including template arguments in a custom error response", func(t *testing.T) {
+		status, contentType, body := check(func(w http.ResponseWriter, r *http.Request) {
+			SetErrorResponse(w, r, http.StatusServiceUnavailable, struct{ Message string }{"Gone to lunch"})
+		})
+
+		assert.Equal(t, http.StatusServiceUnavailable, status)
+		assert.Equal(t, "text/html; charset=utf-8", contentType)
+		assert.Regexp(t, "Service Temporarily Unavailable", body)
+		assert.Regexp(t, "Gone to lunch", body)
 	})
 
 	t.Run("When returning an error directly", func(t *testing.T) {
-		status, contentType := check(func(w http.ResponseWriter, r *http.Request) {
+		status, contentType, body := check(func(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, http.StatusText(http.StatusTeapot), http.StatusTeapot)
 		})
 
 		assert.Equal(t, http.StatusTeapot, status)
 		assert.Equal(t, "text/plain; charset=utf-8", contentType)
+		assert.Regexp(t, "I'm a teapot", body)
 	})
 
 	t.Run("When trying to set an error that we don't have a template for", func(t *testing.T) {
-		status, contentType := check(func(w http.ResponseWriter, r *http.Request) {
+		status, contentType, _ := check(func(w http.ResponseWriter, r *http.Request) {
 			SetErrorResponse(w, r, http.StatusTeapot, nil)
 		})
 
