29f01dc4ad7bdbdda7c727d3cdb2c874b7fd93be
Justin Ko <jko170@gmail.com>
2024-10-27 15:26:14 -0600

    Replace nhooyr.io/websocket with github.com/coder/websocket
    Fixes #52

--- internal/server/target_test.go
+++ internal/server/target_test.go
@@ -12,9 +12,9 @@ import (
 	"testing"
 	"time"
 
+	"github.com/coder/websocket"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"nhooyr.io/websocket"
 )
 
 func TestTarget_Serve(t *testing.T) {
